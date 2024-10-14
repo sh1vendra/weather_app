f587bcd9be0e2299a2f288840c01a66675c3b801
sh1vendra <shivendra@txstate.edu>
2024-10-14 02:48:44 -0500

    Display location and weather data

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -51,6 +51,21 @@ const Weather = () => {
             onKeyPress={search}
           />
         </div>
+
+        {weather.main && (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys && weather.sys.country}
+                <div className="date">{dateBuilder(new Date())}</div>
+              </div>
+              <div className="weather-box">
+                <div className="temp">{Math.round(weather.main.temp)}°C</div>
+                <div className="weather">{weather.weather[0].main}</div>
+              </div>
+            </div>
+          </div>
+        )}
       </main>
     </div>
   );

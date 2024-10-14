68be03418f7cf296e9f49df8f4bb9a71bcd0fedf
sh1vendra <shivendra@txstate.edu>
2024-10-14 02:43:37 -0500

    Setup initial project structure and API config

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+
+const api = {
+  key: "5e8c667fb3e19bde98e6fc191e45aa39",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 const Weather = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
   return (
     <div>
-      Hi
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Weather
+export default Weather;
